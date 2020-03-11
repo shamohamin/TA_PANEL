3abf4ae2e73ac53021af6276f64e884836b4a3c4
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-03-11 20:26:20 +0330

    added userAgent

--- src/data/index.js
+++ src/data/index.js
@@ -2,7 +2,17 @@ import { applyMiddleware , createStore, compose } from "redux";
 import { modelReducer } from './modelReducer' ;
 import { asyncMiddelware } from "./middelware";
 
-export default createStore(modelReducer , compose(
-            applyMiddleware(asyncMiddelware) ,
-            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-            ));
+let store ;
+
+if(navigator.userAgent.indexOf("Chrome") !== -1){
+    store = createStore(modelReducer , compose(
+        applyMiddleware(asyncMiddelware) ,
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    ));
+}else {
+    store = createStore(modelReducer , compose(applyMiddleware(asyncMiddelware)));
+}
+
+
+
+export default store ;
