608a26f647c2b203e524040cfb35319ae8883322
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-03-19 15:46:28 +0330

    chanege url

--- src/data/REST/URLS.js
+++ src/data/REST/URLS.js
@@ -7,7 +7,7 @@ import {USERS , POSTID, POST, GITURL} from '../Types'
 
 export const URLS = {
     [USERS] : `http://apj.ce.kntu.ac.ir:8000/api/users`,
-    [POSTID] : `http://Apj.ce.kntu.ac.ir:8000/api/projects`,
+    [POSTID] : `http://localhost/api/projects`,
     [POST] : `http://Apj.ce.kntu.ac.ir:8000/api/projects`,
     [GITURL] : `http://apj.ce.kntu.ac.ir/git`
 };
