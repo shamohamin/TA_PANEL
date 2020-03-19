32522a9c0820547a414983d04bb9583432f5c57f
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-03-19 14:17:19 +0330

    comment redux devtools

--- src/data/index.js
+++ src/data/index.js
@@ -2,21 +2,22 @@ import { applyMiddleware , createStore, compose } from "redux";
 import { modelReducer } from './modelReducer' ;
 import { asyncMiddelware } from "./middelware";
 
-// let store ;
+let store ;
 
-// if(navigator && navigator.userAgent.indexOf("Chrome") !== -1 ){
-//     store = createStore(modelReducer , compose(
-//         applyMiddleware(asyncMiddelware) ,
-//         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-//     ));
-// }else {
-//     store = createStore(modelReducer , compose(applyMiddleware(asyncMiddelware)));
-// }
+if(navigator && navigator.userAgent.indexOf("Chrome") !== -1 && 
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__){
+    store = createStore(modelReducer , compose(
+        applyMiddleware(asyncMiddelware) ,
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    ));
+}else {
+    store = createStore(modelReducer , compose(applyMiddleware(asyncMiddelware)));
+}
 
 
-// export default store ;
+export default store ;
 
-export default createStore(modelReducer ,
-        compose(applyMiddleware(asyncMiddelware),
+// export default createStore(modelReducer ,
+        // compose(applyMiddleware(asyncMiddelware),
             // window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-        ));
+        // ));
