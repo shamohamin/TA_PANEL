ade2ea91c40c9f027e8ba2b1c15e59eff0112069
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-03-21 16:03:32 +0430

    delete local-server

--- msqlConnection.js
+++
@@ -1,5 +0,0 @@
-const conf = require('./sqlconf') ;
-const mysql = require('mysql');
-const con = mysql.createConnection(conf);
-
-module.exports = con ;
--- server/routes/api.routes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router() ;
-// const apiController = require('../controller/api') ;
-const sqlController = require('../controller/sqlapi') ;
-
-Router.post('/user' , sqlController.MakeUser) ;
-Router.get('/user' , sqlController.getSubmmitionScore) ;
-Router.post('/id', sqlController.just_for_test) ;
-
-module.exports = Router ;
