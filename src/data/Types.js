52746d319de3123ffa9110d4bf8169f284ea74a3
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-04-20 00:30:37 +0430

    added PROJECTPOSTID tu URLS

--- src/data/Types.js
+++ src/data/Types.js
@@ -1,6 +1,7 @@
 export const GETDATA = "GETDATA" ; 
 export const USERS = "USERS" ;
 export const SET_PAGE_SIZE = "SET_PAGE_SIZE";
-export const POSTID = "POSTID" ;
+export const PROJECTPOSTID = "PROJECTPOSTID" ;
+export const HOMEWORKPOSTID = "HOMEWORKPOSTID" ;
 export const POST = "POST" ;
 export const GITURL = "GITURL" ;
