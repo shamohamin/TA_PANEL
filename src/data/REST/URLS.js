96fc3b8a4c54914d474468da845f0e0abdf05133
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-04-20 17:36:53 +0430

    changed url

--- src/data/REST/URLS.js
+++ src/data/REST/URLS.js
@@ -9,7 +9,7 @@ export const URLS = {
     [USERS] : `http://apj.ce.kntu.ac.ir:8000/api/users`,
     [PROJECTPOSTID] : `http://apj.ce.kntu.ac.ir/git/root/hw4-fariborz-hospital-manager-starter.git`,
     [HOMEWORKPOSTID] : `http://apj.ce.kntu.ac.ir/git/root/tahw6-game-starter.git`,
-    [POST] : `http://localhost:8000/api/projects`,
+    [POST] : `http://apj.ce.kntu.ac.ir/api/projects`,
     [GITURL] : `http://apj.ce.kntu.ac.ir/git`
 };
 
